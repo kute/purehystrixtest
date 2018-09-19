e9d6f298120e4d28fd467f8c0d45b19a7d328ff8
白龙(23119351) <bailong001@ke.com>
2018-09-19 10:38:09 +0800

    first commit

---
+++ src/main/java/com/kute/hystrix/PurehystrixtestApplication.java
@@ -0,0 +1,12 @@
+package com.kute.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurehystrixtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurehystrixtestApplication.class, args);
+	}
+}
